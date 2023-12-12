9d0f30f3c0e2a70c9c219b94a5137a6d23dd47ab
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2023-12-12 18:19:14 +0530

    sign in/sign up button links working

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
-import Image from 'next/image'
+
 import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 
 
 
 export default function Home() {
+
+  const handleButtonClick = () => {
+    console.log('Button clicked!');
+    router.push('/inputPage.js');
+  };
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* ...existing code... */}
@@ -18,10 +26,10 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center space-y-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={<LoginLink className="text-blue-500 underline">Sign in</LoginLink>}>Sign in</button>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={ <RegisterLink className="text-blue-500 underline">Sign up</RegisterLink>}>Sign up</button>
+       <LoginLink className="bg-blue-500 text-white px-4 py-2 rounded-md" postLoginRedirectURL="/dashboard">Sign in</LoginLink>
+       <RegisterLink className="bg-blue-500 text-white px-4 py-2 rounded-md">Sign up</RegisterLink>
        
-        <Link href="/inputPage"className="bg-blue-500 text-white px-4 py-2 rounded-md">Go to Input Page
+        <Link href="/inputPage.js"className="bg-blue-500 text-white px-4 py-2 rounded-md" >Go to Input Page
         </Link>
         </div>
 
